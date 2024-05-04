a7b63ec15e7bb754f2727ef7341d776951e68537
MagdalitaAhmad <127321597+MagdalitaAhmad@users.noreply.github.com>
2024-05-04 21:56:59 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+import DataForm from "./assets/DataForm"
+
+function App() {
+
+
+  return (
+    <>
+      <DataForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/DataForm.jsx
@@ -0,0 +1,133 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+const DataForm = () => {
+    const [data, setData] = useState([]);
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [error, setError] = useState(null);
+    const [editItem, setEditItem] = useState(null)
+
+    useEffect(() => {
+        axios.get('https://whimsical-kitten-77110a.netlify.app/.netlify/functions/api')
+            .then((response) => {
+                setData(response.data);
+            })
+            .catch((error) => {
+                setError('Error occurred while fetching data: ' + error.message);
+            });
+    }, []);
+
+    const handleSubmit = (e, id = null) => {
+        e.preventDefault();
+        if (!name || !age) {
+            setError('Name and age are required');
+            return;
+        }
+
+        const url = editItem
+            ? `https://whimsical-kitten-77110a.netlify.app/.netlify/functions/api/${editItem._id}`
+            : `https://whimsical-kitten-77110a.netlify.app/.netlify/functions/api`;
+        const method = editItem ? 'put' : 'post'
+        
+        axios[method](url, { name, age })
+            .then((response) => {
+                console.log(response.data);
+
+                if (editItem) {
+                    setData(
+                        data.map((item) =>
+                            item._id === id ? response.data : item));
+                } else {
+                    setData([...data, response.data.author]);
+                }
+                setName('');
+                setAge('');
+                setEditItem(null);
+                setError(null);
+            })
+            .catch((error) => {
+                setError('Error occurred while submitting data: ' + error.message);
+            });
+    }
+
+    const handleEdit = (_id) => {
+        const itemToEdit = data.find((item) => item._id === _id);
+        setEditItem(itemToEdit);
+        setName(itemToEdit.name);
+        setAge(itemToEdit.age);
+    }
+
+
+    const handleDelete = (_id) => {
+        axios
+            .delete(
+                `https://whimsical-kitten-77110a.netlify.app/.netlify/functions/api/${_id}`
+            )
+            .then(() => {
+                setData(data.filter((item) => item._id !== _id));
+            })
+            .catch((error) => {
+                console.log('Error occurred while deleting data: ' , error.message);
+            });
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit} className="form">
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Name"
+                    className="input-field"
+                />
+                <input
+                    type="text"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                    placeholder="Age"
+                    className="input-field"
+                />                 
+                <button type="submit" className="submit-btn"> {editItem ?  'Update Data' : 'Add Data'}</button>
+            </form>
+            {error && <p>{error}</p>}
+            
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item) => (
+                        <tr key={item._id}>
+                            <td>{item.name}</td>
+                            <td>{item.age}</td>
+                            <td>
+                                <button onClick={() => handleEdit(item._id)} className="edit-btn">Edit</button>
+                                <button onClick={() => handleDelete(item._id)} className="delete-btn">Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            {/* <ul>
+                {data.map((item) => (
+                    <li key={item._id}>
+                        {item.name} - {item.age}
+                        <button onClick={() => handleEdit(item._id)} className="edit-btn">Edit</button>
+                        <button onClick={() => handleDelete(item._id)} className="delete-btn">Delete</button>
+                    </li>
+                ))}
+            </ul> */}
+
+        </div>
+    );
+}
+
+export default DataForm;
